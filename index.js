a3b9ef0a60b0b1f9c4a789029efb9ff5162e60f4
SboneloMdluli <1101772@students.wits.ac.za>
2019-04-18 22:30:52 +0200

    new commit

---
+++ index.js
@@ -0,0 +1,10 @@
+let express = require('express')
+let app = express()
+//loading our routers
+let mainRouter = require('./mainRoutes.js')
+let classRouter = require('./classRoutes.js')
+//mounting our routers
+app.use('/', mainRouter)
+app.use('/class', classRouter)
+app.listen(3000)
+console.log('Express server running on port 3000')
---
+++ mainRoutes.js
@@ -0,0 +1,10 @@
+let path = require('path')
+let express = require('express')
+let mainRouter = express.Router()
+mainRouter.get('/', function (req, res) {
+  res.send('Hello World')
+})
+mainRouter.get('/', function (req, res) {
+  res.send('Hello World, I\'m Node.js')
+})
+module.exports = mainRouter
