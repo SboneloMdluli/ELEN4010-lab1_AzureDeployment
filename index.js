10683265cbf371f5f45167fd70c2145d439ec2d2
SboneloMdluli <1101772@students.wits.ac.za>
2019-04-22 20:18:44 +0200

    nodedeploy

--- index.js
+++ index.js
@@ -6,5 +6,6 @@ let classRouter = require('./classRoutes.js')
 // mounting our routers
 app.use('/', mainRouter)
 app.use('/class', classRouter)
-app.listen(3000)
-console.log('Express server running on port 3000')
+let port = process.env.PORT || 3000
+app.listen(port)
+console.log('Express server running on port', port)
